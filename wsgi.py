74f1f9924cba65d8632f6543e486e9298e7a1f13
jbaudry@gmail.com <jbaudry@gmail.com>
2018-07-13 10:58:23 +0200

    blabla

--- wsgi.py
+++ wsgi.py
@@ -5,7 +5,7 @@ try:
 except:
     pass # Heroku does not use .env
 
-from flask import Flask
+from flask import Flask, request
 from config import Config
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -18,6 +18,7 @@ ma = Marshmallow(app)
 
 from models import Product
 from schemas import products_schema
+from schemas import product_schema
 
 @app.route('/')
 def hello():
@@ -29,8 +30,31 @@ def products():
     return products_schema.jsonify(products)
 
 @app.route('/api/v1/products/<id>')
-def get_product():
+def get_product(id):
     product = db.session.query(Product).get(int(id))
-    return products_schema.jsonify(product)
+    return product_schema.jsonify(product)
 
+@app.route('/api/v1/products', methods=['POST'])
+def add_product():
+    new_product = Product()
+    new_product.name = request.form['name']
+    new_product.description = request.form['description']
+    db.session.add(new_product)
+    db.session.commit()
+    return product_schema.jsonify(new_product)
+
+@app.route('/api/v1/products', methods=['PATCH'])
+def update_product():
+    new_product = Product()
+    new_product.name = request.form['name']
+    new_product.description = request.form['description']
+    db.session.query(Product).filter_by(name=new_product.name).update({"description" : new_product.description})
+    db.session.commit()
+    return product_schema.jsonify(new_product)
+
+@app.route('/api/v1/products/<id>', methods=['DELETE'])
+def delete_product(id):
+    db.session.query(Product).filter_by(id=id).delete()
+    db.session.commit()
+    return ('', 200)
 
